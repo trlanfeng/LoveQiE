40a44116d8bd405f4f12ea404527ac8a9e699364
孤月蓝风 <trlanfeng@foxmail.com>
2016-06-09 02:51:58 +0800

    心加入呼吸动画 版本升级

---
+++ Assets/BreathIt.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class BreathIt : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.DOScale(1f, 0.5f).SetLoops(100,LoopType.Yoyo).SetEase(Ease.InBack);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
